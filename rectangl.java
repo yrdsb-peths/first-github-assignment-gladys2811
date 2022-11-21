2269344d9586b8c9a62392a477ff37886c1ef4c8
gladys2811 <117942158+gladys2811@users.noreply.github.com>
2022-11-21 14:44:59 -0500

    Update rectangl.java

--- rectangl.java
+++ rectangl.java
@@ -52,6 +52,32 @@ public class Rectangle
     }
 }
     
-    
+    public class StudentTester extends ConsoleProgram
+
+{
+
+    public void run()
+
+    {
+
+        Student alan = new Student("Alan", "Turing", 11);
+
+        Student ada = new Student("Ada", "Lovelace", 12);
+
+        // Add an entry for yourself here!
+
+        Student gladys = new Student ("Gladys", "Lee", 11);
+
+        System.out.println(alan);
+
+        System.out.println(ada);
+
+        // Print out your student object
+
+        System.out.println(gladys);
+
+    }
+
+}
     
 }
